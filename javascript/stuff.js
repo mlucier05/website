f2568e47261ae7e7c9344e6cd1f0e926e07f411b
MotorGorilla <59579441+MotorGorilla1@users.noreply.github.com>
2021-12-03 08:04:44 -0500

    Add comments to stuff.js

--- javascript/stuff.js
+++ javascript/stuff.js
@@ -1,41 +1,34 @@
 let triangle = "#";
 
-for(let counter = 0; counter < 7; counter++)
-{
- 	console.log(triangle);
-  	triangle = triangle + "#";
+//Triangle
+for (let counter = 0; counter < 7; counter++) {
+  console.log(triangle);
+  triangle = triangle + "#";
 }
 
-for(let i = 1; i < 101; i++)
-{
- 	if(i % 3 == 0 && i % 5 == 0)
-    {
-     	console.log("FizzBuzz");
-    }else if(i % 3 == 0)
-    {
-     	console.log("Fizz");
-    }else if(i % 5 == 0)
-    {
-     	console.log("Buzz");
-    }else
-    {
-     	console.log(i);
-    }
+//FizzBuzz
+for (let i = 1; i < 101; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
 
+//Chessboard
 let size = 6;
 let chessboard = "";
 
-for(let height = 0; height < size; height++)
-{
-  for(let length = 0; length < size/2; length++)
-  {
-    if(height % 2 == 1)
-    {
-    	chessboard += "#";
-    }else
-    {
-      	chessboard += " ";
+for (let height = 0; height < size; height++) {
+  for (let length = 0; length < size / 2; length++) {
+    if (height % 2 == 1) {
+      chessboard += "#";
+    } else {
+      chessboard += " ";
     }
   }
   chessboard += "\n";
