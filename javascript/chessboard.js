ef5f02db5bf81e036e61a1fc947a6a6a59318449
MotorGorilla <59579441+MotorGorilla1@users.noreply.github.com>
2022-02-11 08:31:51 -0500

    Seperate js projects and add chapter 3 notes

---
+++ javascript/chessboard.js
@@ -0,0 +1,16 @@
+//Chessboard
+let size = 6;
+let chessboard = "";
+
+for (let height = 0; height < size; height++) {
+  for (let length = 0; length < size / 2; length++) {
+    if (height % 2 == 1) {
+      chessboard += "#";
+    } else {
+      chessboard += " ";
+    }
+  }
+  chessboard += "\n";
+}
+
+console.log(chessboard);
---
+++ javascript/triangle.js
@@ -0,0 +1,8 @@
+//Triangle
+
+let triangle = "#";
+
+for (let counter = 0; counter < 7; counter++) {
+  console.log(triangle);
+  triangle = triangle + "#";
+}
